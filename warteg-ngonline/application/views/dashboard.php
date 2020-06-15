72a32da7f8e34fdd9e6f267299c87588d499b21a
ikkyutioktano <63623036+ikkyutioktano@users.noreply.github.com>
2020-06-15 15:58:13 +0700

    tugas uas web 3

--- warteg-ngonline/application/views/dashboard.php
+++ warteg-ngonline/application/views/dashboard.php
@@ -3,7 +3,7 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="<?php echo base_url('assets/img/warteg.png') ?>" class="d-block w-100" alt="...">
+      <img src="<?php echo base_url('assets/img/wartegngonline1.jpg') ?>" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
       <img src="<?php echo base_url('assets/img/warteg1.png') ?>" class="d-block w-100" alt="...">
